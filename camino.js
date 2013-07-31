08c0b7305aa62dc752f70d49d5df75edf3156aee
Ross Paskett <ross@paskett.me>
2013-07-31 01:22:44 -0600

    Trying to get the listeners to work properly.  No idea how/which listener to implement for node.js

--- camino.js
+++ camino.js
@@ -1,28 +1,60 @@
 (function() {
-	var routes = {};
-	var root = this;
-
 	// route is a URL or a hash
 	// callback is the function to execute when route is matched
 	// context is a filter of some kind. GET-POST-PUT-DELETE on the server,
 	// arbitrary in the browser
-	function Camino( route, callback, context ) {
-		routes[route] = { callback: callback, context: context };
-	}
+	var routes = {},
+		root = this,
+		emitter = null,
+		event = null;
 
-	function camino( route, callback, context ) {
-		return new Camino( route, callback, context );
-	}
+	function Camino() { };
+	function camino() { return new Camino; };
 
 	if( typeof module !== 'undefined' && module.exports ) {
-		module.exports = route;
+		module.exports = camino;
+		// set listener for http/s server
+		// emitter = root.on ?
 	}
 
 	else {
-		window.onhashchange = function() {
-
-		}
+		root.camino = camino;
+		emitter = root.addEventListener;
+		event = 'hashchange';
+		req = function() { return root.location.hash };
 	}
 
-	root.camino = camino;
+
+	camino.fn = Camino.prototype = {
+		route: function( route, callback, context ) {
+			routes[route] = { callback: callback, context: context };
+		},
+
+		// this function may have no practical use, but for testing/dev it's here
+		list: function() {
+			var r = [], ii = 0;
+			for( r[ii++] in routes );
+			return r;
+		},
+
+		call: function( route, args ) {
+			args = args || [];
+
+			console.log("route: " + route);
+
+			if( routes[route] !== undefined )
+			{
+				return routes[route].callback.apply(null, args);
+			}
+			// else
+			// 	throw new Error( 'Route not found.' );
+		},
+
+		listen: function() {
+			var c = this;
+			emitter( event, function() {
+				c.call( req() );
+			} );
+		}
+	};
 })();
